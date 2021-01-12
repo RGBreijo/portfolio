632633250e47fa5d1fc12397378cb0b364e94e9b
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2021-01-11 20:31:10 -0500

    minor change

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ hamburger.addEventListener('click', navBarClicked);
 function navBarClicked()
 {
     let nav = document.getElementsByTagName("nav")[0];
-    let style = window.getComputedStyle(nav);
 
     if(nav.style.height == "0px")
     {
-          nav.style.display = "block";
           nav.style.height = "150px";
           nav.style.transition = "all 0.5s";
     }else{
